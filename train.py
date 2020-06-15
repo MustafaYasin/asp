2b8c7f45b451b5c188c9ec0aea82abb698e62ad9
leo-mao <emicool.my@gmail.com>
2020-06-15 18:12:53 +0800

    init algo and unity env

---
+++ train.py
@@ -0,0 +1,6 @@
+from mlagents_envs.environment import UnityEnvironment
+# This is a non-blocking call that only loads the environment.
+env = UnityEnvironment(file_name="tennis.x86_64", seed=1, side_channels=[])
+# Start interacting with the evironment.
+env.reset()
+behavior_names = env.behavior_spec.keys()
