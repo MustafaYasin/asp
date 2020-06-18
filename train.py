0feb64ec9d4f4a0a0f1db096ba8d9e357275957c
marcchan <xjmarc.chen@icloud.com>
2020-06-18 14:54:51 +0800

    add macOS loading option

--- train.py
+++ train.py
@@ -1,6 +1,9 @@
 from mlagents_envs.environment import UnityEnvironment
 # This is a non-blocking call that only loads the environment.
 env = UnityEnvironment(file_name="tennis.x86_64", seed=1, side_channels=[])
+# mac binary file
+# env = UnityEnvironment(file_name="tennis.app", seed=1, side_channels=[])
+
 # Start interacting with the evironment.
 env.reset()
 behavior_names = env.behavior_spec.keys()
