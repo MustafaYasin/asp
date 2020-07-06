959d05c94ab6d1f881c9e7bc71db95cae47fb2eb
marcchan <xingjian.chen@campus.lmu.de>
2020-07-06 22:00:32 +0800

    create a new branch with mlagent v= 0.4.0

---
+++ eval.py
@@ -0,0 +1,31 @@
+from unityagents import UnityEnvironment
+import numpy as np
+import torch
+from asp.ddpg_agent import Agent
+
+random_seed = 7
+
+
+env = UnityEnvironment(file_name="Tennis_old.app")
+agent = Agent(state_size=24, action_size=2, random_seed=random_seed)
+agent.actor_local.load_state_dict(torch.load('checkpoint_actor.pth', map_location='cpu'))
+agent.critic_local.load_state_dict(torch.load('checkpoint_critic.pth', map_location='cpu'))
+brain_name = env.brain_names[0]
+env_info = env.reset(train_mode=True)[brain_name]
+num_agents = len(env_info.agents)
+for i in range(100):                                         # play game for 5 episodes
+    env_info = env.reset(train_mode=False)[brain_name]     # reset the environment
+    states = env_info.vector_observations                  # get the current state (for each agent)
+    scores = np.zeros(num_agents)                          # initialize the score (for each agent)
+    while True:
+        actions = agent.act(states)                        # select actions from loaded model agent
+        actions = np.clip(actions, -1, 1)                  # all actions between -1 and 1
+        env_info = env.step(actions)[brain_name]           # send all actions to tne environment
+        next_states = env_info.vector_observations         # get next state (for each agent)
+        rewards = env_info.rewards                         # get reward (for each agent)
+        dones = env_info.local_done                        # see if episode finished
+        scores += env_info.rewards                         # update the score (for each agent)
+        states = next_states                               # roll over states to next time step
+        if np.any(dones):                                  # exit loop if episode finished
+            break
+    print('Total score (averaged over agents) this episode: {}'.format(np.mean(scores)))
--- train.py
+++ train.py
@@ -1,29 +1,105 @@
-from mlagents_envs.environment import UnityEnvironment
+from unityagents import UnityEnvironment
 import numpy as np
-# This is a non-blocking call that only loads the environment.
-# env = UnityEnvironment(file_name="tennis.x86_64", seed=1, side_channels=[])
-# mac binary file
-env = UnityEnvironment(file_name="tennis.app", seed=1, side_channels=[])
-# env = UnityEnvironment(file_name="tennis_demo.app", seed=1, side_channels=[], )
-# Start interacting with the evironment.
-env.reset()
-behavior_names = env.get_behavior_names()
-brain_1 = env.get_behavior_names()[0]
-# brain_2 = env.get_behavior_names()[1]
-
-for behavior_name in behavior_names:
-  behavior_specs=env.get_behavior_spec(behavior_name)
-  print(str(behavior_name) + " " + str(behavior_specs))
-  
-for _ in range(1000):
-  env.step()
-  decision_steps, terminal_steps = env.get_steps(brain_1)
-  print(f'decision_steps:{decision_steps.obs[0].shape}')
-  print(list(terminal_steps.keys()))
-  # if terminal_steps.keys
-  if(len(list(terminal_steps.keys())) == 0):env.set_actions(behavior_name=brain_1, action=np.random.randn(18, 3))
+from asp.algo.ddpg_agent import Agent
+import torch
+import random
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import deque
 
 
-env.close()
+env = UnityEnvironment(file_name="Tennis_old.app", no_graphics= True)
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+# number of agents
+num_agents = len(env_info.agents)
+print('Number of agents:', num_agents)
+# size of each action
+action_size = brain.vector_action_space_size
+print('Size of each action:', action_size)
+# examine the state space
+states = env_info.vector_observations
+state_size = states.shape[1]
+print('There are {} agents. Each observes a state with length: {}'.format(states.shape[0], state_size))
+print('The state for the first agent looks like:', states[0])
+random_seed = 7
+train_mode = True
+# Create one brain agent having one Reply memory buffer collecting experience from both tennis agents
+agent = Agent(state_size=state_size, action_size=action_size, random_seed=random_seed)
+
+def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=5, num_learn=10, goal_score=0.5):
+  total_scores_deque = deque(maxlen=100)
+  total_scores = []
+
+  for i_episode in range(1, n_episodes + 1):
+
+    # Reset Env and Agent
+    env_info = env.reset(train_mode=train_mode)[brain_name]  # reset the environment
+    states = env_info.vector_observations  # get the current state (for each agent)
+    scores = np.zeros(num_agents)  # initialize the score (for each agent)
+    agent.reset()
+    
+    start_time = time.time()
+
+    for t in range(max_t):
+      actions = agent.act(states)
+      print('actions:{}'.format(actions))
+      env_info = env.step(actions)[brain_name]  # send all actions to the environment
+      next_states = env_info.vector_observations  # get next state (for each agent)
+      rewards = env_info.rewards  # get reward (for each agent)
+      # print(rewards)
+      dones = env_info.local_done  # see if episode finished
+      print('dones:{}'.format(dones))
+      for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+        agent.step(state, action, reward, next_state, done)  # send actions to the agent
+
+      scores += env_info.rewards  # update the score (for each agent)
+      states = next_states  # roll over states to next time step
+      
+      if t % learn_every == 0:
+        for _ in range(num_learn):
+          agent.start_learn()
+
+      if np.any(dones):  # exit loop if episode finished
+        break
+
+    mean_score = np.mean(scores)
+    min_score = np.min(scores)
+    max_score = np.max(scores)
+
+    # Take max of all agents' scores
+    total_scores_deque.append(max_score)
+    total_scores.append(max_score)
+    total_average_score = np.mean(total_scores_deque)
+    duration = time.time() - start_time
+
+    if i_episode % print_every == 0:
+      print(
+        '\rEpisode {}\tTotal Average Score: {:.2f}\tMean: {:.2f}\tMin: {:.2f}\tMax: {:.2f}\tDuration: {:.2f}'.format(
+          i_episode, total_average_score, mean_score, min_score, max_score, duration))
+
+    if i_episode % save_every == 0:
+      torch.save(agent.actor_local.state_dict(), 'pthLog/checkpoint_actor_{}.pth'.format(i_episode))
+      torch.save(agent.critic_local.state_dict(), 'pthLog/checkpoint_critic_{}.pth'.format(i_episode))
+
+    if total_average_score >= goal_score and i_episode >= 100:
+      print('Problem Solved after {} epsisodes!! Total Average score: {:.2f}'.format(i_episode, total_average_score))
+      torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
+      torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+      break
+
+  return total_scores
 
 
+scores = ddpg()
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+
+plt.plot(np.arange(1, len(scores)+1), scores)
+plt.ylabel('Average Score')
+plt.xlabel('Episode No.')
+plt.show()
