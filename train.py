6c38410231ba9cd0b4f0a344df8e2fa0d9bddf17
leo-mao <emicool.my@gmail.com>
2020-07-08 15:09:31 +0800

    change the pytorch save style

--- train.py
+++ train.py
@@ -1,14 +1,17 @@
-from unityagents import UnityEnvironment
-import numpy as np
-from algo.ddpg_agent import Agent
 import torch
 import time
 import numpy as np
 import matplotlib.pyplot as plt
+
+from datetime import datetime
+from os import makedirs
+from unityagents import UnityEnvironment
+from algo.ddpg_agent import Agent
 from collections import deque
 
-
-env = UnityEnvironment(file_name="Tennis_Linux/Tennis.x86_64", no_graphics= True)
+# For linux
+env = UnityEnvironment(file_name="Tennis_Linux/Tennis.x86_64", no_graphics=True)
+# For mac os
 # env = UnityEnvironment(file_name="Tennis_old.app")
 
 # get the default brain
@@ -29,12 +32,19 @@ print('There are {} agents. Each observes a state with length: {}'.format(states
 print('The state for the first agent looks like:', states[0])
 random_seed = 7
 train_mode = True
+
 # Create one brain agent having one Reply memory buffer collecting experience from both tennis agents
 agent = Agent(state_size=state_size, action_size=action_size, random_seed=random_seed)
+save_dir = datetime.now().strftime("%m%d_%H:%M")
+makedirs(save_dir, exist_ok=True)
 
-def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=5, num_learn=10, goal_score=0.5):
+
+def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=5, num_learn=10, goal_score=0.7):
+
   total_scores_deque = deque(maxlen=100)
   total_scores = []
+  actor_losses = []
+  critic_losses = []
 
   for i_episode in range(1, n_episodes + 1):
 
@@ -42,6 +52,7 @@ def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=
     env_info = env.reset(train_mode=train_mode)[brain_name]  # reset the environment
     states = env_info.vector_observations  # get the current state (for each agent)
     scores = np.zeros(num_agents)  # initialize the score (for each agent)
+
     agent.reset()
     
     start_time = time.time()
@@ -63,7 +74,9 @@ def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=
       
       if t % learn_every == 0:
         for _ in range(num_learn):
-          agent.start_learn()
+          a_l, c_l = agent.start_learn()
+          actor_losses.append(a_l * 1e3)
+          critic_losses.append(c_l * 1e3)
 
       if np.any(dones):  # exit loop if episode finished
         break
@@ -78,19 +91,32 @@ def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=
     total_average_score = np.mean(total_scores_deque)
     duration = time.time() - start_time
 
+
     if i_episode % print_every == 0:
       print(
         '\rEpisode {}\tTotal Average Score: {:.2f}\tMean: {:.2f}\tMin: {:.2f}\tMax: {:.2f}\tDuration: {:.2f}'.format(
           i_episode, total_average_score, mean_score, min_score, max_score, duration))
 
     if i_episode % save_every == 0:
-      torch.save(agent.actor_local.state_dict(), 'checkpoint_actor_{}.pth'.format(i_episode))
-      torch.save(agent.critic_local.state_dict(), 'checkpoint_critic_{}.pth'.format(i_episode))
 
-    if total_average_score >= goal_score and i_episode >= 100:
+      torch.save({'episode': i_episode,
+                  'actor_static': agent.actor_local.state_dict(),
+                  'actor_loss': actor_losses,
+                  'critic_static': agent.critic_local.state_dict(),
+                  'critic_loss': critic_losses,
+                  'total_score': total_scores
+                  }, '{}/a-c_{}.pth'.format(save_dir, i_episode))
+
+
+    if total_average_score >= 0.8 and i_episode >= 100:
       print('Problem Solved after {} epsisodes!! Total Average score: {:.2f}'.format(i_episode, total_average_score))
-      torch.save(agent.actor_local.state_dict(), 'checkpoint_actor.pth')
-      torch.save(agent.critic_local.state_dict(), 'checkpoint_critic.pth')
+      torch.save({'episode': i_episode,
+                  'actor_static': agent.actor_local.state_dict(),
+                  'actor_loss': actor_losses,
+                  'critic_static': agent.critic_local.state_dict(),
+                  'critic_loss': critic_losses,
+                  'total_score': total_scores
+                  }, '{}/a-c_{}.pth'.format(save_dir, i_episode))
       break
 
   return total_scores
