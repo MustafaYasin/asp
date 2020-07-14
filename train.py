96db78f88ab65d1e38d409317b6fc469e6b880f4
Yang Mao <mao.yang@campus.lmu.de>
2020-07-15 00:26:42 +0800

    continue training with existed pth file finetuned step number

--- train.py
+++ train.py
@@ -36,12 +36,15 @@ random_seed = 7
 train_mode = True
 # Create one brain agent having one Reply memory buffer collecting experience from both tennis agents
 agent = Agent(state_size=state_size, action_size=action_size, random_seed=random_seed)
-
 save_dir = datetime.now().strftime("%m%d_%H:%M")
 makedirs(save_dir, exist_ok=True)
+continue_train = True
+if continue_train:
+    trained = torch.load("a-c_2695-j244928-3.pth", map_location='cpu')
+    agent.actor_local.load_state_dict(trained['actor_static'])
+    agent.critic_local.load_state_dict(trained['critic_static'])
 
-
-def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=5, num_learn=10, goal_score=0.7):
+def ddpg(n_episodes=5000, max_t=5000, print_every=5, save_every=50, learn_every=5, num_learn=10, goal_score=0.7):
 
   total_scores_deque = deque(maxlen=100)
   total_scores = []
@@ -85,21 +88,21 @@ def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=
       if np.any(dones):  # exit loop if episode finished
         break
 
-    mean_score = np.mean(scores)
-    min_score = np.min(scores)
     max_score = np.max(scores)
 
     # Take max of all agents' scores
     total_scores_deque.append(max_score)
     total_scores.append(max_score)
+
+    # mean total scores of last 100 episodes
     total_average_score = np.mean(total_scores_deque)
     duration = time.time() - start_time
 
 
     if i_episode % print_every == 0:
       print(
-        '\rEpisode {}\tTotal Average Score: {:.2f}\tMean: {:.2f}\tMin: {:.2f}\tMax: {:.2f}\tDuration: {:.2f}'.format(
-          i_episode, total_average_score, mean_score, min_score, max_score, duration))
+        '\rEpisode {}\tTotal Average Score: {:.2f}\tMax: {:.2f}\tDuration: {:.2f}'.format(
+          i_episode, total_average_score, max_score, duration))
 
     if i_episode % save_every == 0:
 
@@ -111,7 +114,7 @@ def ddpg(n_episodes=2000, max_t=2000, print_every=5, save_every=50, learn_every=
                   'total_score': total_scores
                   }, '{}/a-c_{}.pth'.format(save_dir, i_episode))
 
-    if total_average_score >= goal_score and i_episode >= 100:
+    if total_average_score >= goal_score and i_episode >= 1000:
       print('Problem Solved after {} epsisodes!! Total Average score: {:.2f}'.format(i_episode, total_average_score))
       torch.save({'episode': i_episode,
                   'actor_static': agent.actor_local.state_dict(),
