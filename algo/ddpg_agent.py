0e1b7175c9f504dfcff0879968708b1d89a1b29b
Yang Mao <mao.yang@campus.lmu.de>
2020-07-17 15:31:00 +0800

    fix intent problem

--- algo/ddpg_agent.py
+++ algo/ddpg_agent.py
@@ -84,64 +84,64 @@ class Agent:
       a_l, c_l = self.learn(experiences, GAMMA)
       if not self.start_learning:
         self.start_learning = True
-        print('*'*10+"start learning")
+        print('*'*10+"start learning"+'*'*10)
     return (a_l, c_l) if a_l and c_l else (0, 0)
 
-def learn(self, experiences, gamma):
-  """Update policy and value parameters using given batch of experience tuples.
-  Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
-  where:
-      actor_target(state) -> action
-      critic_target(state, action) -> Q-value
+  def learn(self, experiences, gamma):
+    """Update policy and value parameters using given batch of experience tuples.
+    Q_targets = r + γ * critic_target(next_state, actor_target(next_state))
+    where:
+        actor_target(state) -> action
+        critic_target(state, action) -> Q-value
 
-  Params
-  ======
-      experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
-      gamma (float): discount factor
-  """
-  states, actions, rewards, next_states, dones = experiences
+    Params
+    ======
+        experiences (Tuple[torch.Tensor]): tuple of (s, a, r, s', done) tuples
+        gamma (float): discount factor
+    """
+    states, actions, rewards, next_states, dones = experiences
 
-  # ---------------------------- update critic ---------------------------- #
-  # Get predicted next-state actions and Q values from target models
-  actions_next = self.actor_target(next_states)
-  Q_targets_next = self.critic_target(next_states, actions_next)
-  # Compute Q targets for current states (y_i)
-  Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
-  # Compute critic loss
-  Q_expected = self.critic_local(states, actions)
-  critic_loss = F.mse_loss(Q_expected, Q_targets)
-  # Minimize the loss
-  self.critic_optimizer.zero_grad()
-  critic_loss.backward()
-  torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)
-  self.critic_optimizer.step()
+    # ---------------------------- update critic ---------------------------- #
+    # Get predicted next-state actions and Q values from target models
+    actions_next = self.actor_target(next_states)
+    Q_targets_next = self.critic_target(next_states, actions_next)
+    # Compute Q targets for current states (y_i)
+    Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+    # Compute critic loss
+    Q_expected = self.critic_local(states, actions)
+    critic_loss = F.mse_loss(Q_expected, Q_targets)
+    # Minimize the loss
+    self.critic_optimizer.zero_grad()
+    critic_loss.backward()
+    torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)
+    self.critic_optimizer.step()
 
-  # ---------------------------- update actor ---------------------------- #
-  # Compute actor loss
-  actions_pred = self.actor_local(states)
-  actor_loss = -self.critic_local(states, actions_pred).mean()
-  # Minimize the loss
-  self.actor_optimizer.zero_grad()
-  actor_loss.backward()
-  self.actor_optimizer.step()
+    # ---------------------------- update actor ---------------------------- #
+    # Compute actor loss
+    actions_pred = self.actor_local(states)
+    actor_loss = -self.critic_local(states, actions_pred).mean()
+    # Minimize the loss
+    self.actor_optimizer.zero_grad()
+    actor_loss.backward()
+    self.actor_optimizer.step()
 
-  # ----------------------- update target networks ----------------------- #
-  self.soft_update(self.critic_local, self.critic_target, TAU)
-  self.soft_update(self.actor_local, self.actor_target, TAU)
-  return actor_loss, critic_loss
+    # ----------------------- update target networks ----------------------- #
+    self.soft_update(self.critic_local, self.critic_target, TAU)
+    self.soft_update(self.actor_local, self.actor_target, TAU)
+    return actor_loss, critic_loss
 
-def soft_update(self, local_model, target_model, tau):
-  """Soft update model parameters.
-  θ_target = τ*θ_local + (1 - τ)*θ_target
+  def soft_update(self, local_model, target_model, tau):
+    """Soft update model parameters.
+    θ_target = τ*θ_local + (1 - τ)*θ_target
 
-  Params
-  ======
-      local_model: PyTorch model (weights will be copied from)
-      target_model: PyTorch model (weights will be copied to)
-      tau (float): interpolation parameter
-  """
-  for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
-    target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
+    Params
+    ======
+        local_model: PyTorch model (weights will be copied from)
+        target_model: PyTorch model (weights will be copied to)
+        tau (float): interpolation parameter
+    """
+    for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+      target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
 
 
 class OUNoise:
